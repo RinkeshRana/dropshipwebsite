4ed30448f0c69336d0eecb632486aaa53d4a920b
RinkeshRana <rinkeshr007@gmail.com>
2023-05-20 19:39:28 +0530

    modified:   app/components/Product.jsx 	new file:   app/product/[slug]/page.jsx

--- app/components/Product.jsx
+++ app/components/Product.jsx
@@ -21,24 +21,26 @@ function Product({
 
   return (
     <div class="w-64 h-96 md:w-80 md:h-112 flex flex-col justify-center bg-white rounded-lg  hover:cursor-pointer">
-      <div class="relative h-80 items-center">
-        <Image
-          src={imageSrc}
-          alt="Product image"
-          width={500}
-          height={500}
-          class="w-full h-full object-contain rounded-lg transform hover:scale-110 transition-transform duration-500 fade"
-          onMouseEnter={handleHover}
-          onMouseLeave={handleHover}
-        />
-        <div class="absolute top-0 left-0 bg-red-500 text-white font-extrabold px-2 py-1 rounded-full text-xs">
-          -20%
+      <Link href={"/product/" + productName}>
+        <div class="relative h-80 items-center">
+          <Image
+            src={imageSrc}
+            alt="Product image"
+            width={500}
+            height={500}
+            class="w-full h-full object-contain rounded-lg transform hover:scale-110 transition-transform duration-500 fade"
+            onMouseEnter={handleHover}
+            onMouseLeave={handleHover}
+          />
+          <div class="absolute top-0 left-0 bg-red-500 text-white font-extrabold px-2 py-1 rounded-full text-xs">
+            -20%
+          </div>
         </div>
-      </div>
-      <div class="mt-4 text-gray-800 text-base font-bold uppercase">
-        Product name
-      </div>
-      <div class="mt-2 text-gray-600 font-medium">$19.99</div>
+        <div class="mt-4 text-gray-800 text-base font-bold uppercase">
+          Product name
+        </div>
+        <div class="mt-2 text-gray-600 font-medium">$19.99</div>
+      </Link>
     </div>
   );
 }
